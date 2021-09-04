c833aadae9f923fcff77fd96a1c4456751834238
alensari <87225299+alensari@users.noreply.github.com>
2021-09-04 16:50:46 +0200

    dodatak css

--- Portal/Portal/urls.py
+++ Portal/Portal/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('diskusija/', include('diskutovanje.urls')),
     path('ankete/', include('ankete.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'))
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("login/", LoginView.as_view(template_name = "registration/login.html"), name = "login"),
+    path("", TemplateView.as_view(template_name = "Portal_Za_Volontiranje.html"), name = "index"),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
