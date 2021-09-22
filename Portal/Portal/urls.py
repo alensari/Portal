c7b70e70918c58e468efab485dd7b4a010a0000f
alensari <87225299+alensari@users.noreply.github.com>
2021-09-22 18:53:32 +0200

    Povratak Portala

--- Portal/Portal/urls.py
+++ Portal/Portal/urls.py
@@ -22,15 +22,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    #path('diskusija/', include('diskutovanje.urls')),
+    path('diskusija/', include('diskutovanje.urls')),
     path('ankete/', include('ankete.urls')),
-    #path('accounts/', include('django.contrib.auth.urls')),
-    #path("login/", LoginView.as_view(template_name = "registration/login.html"), name = "login"),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("login/", LoginView.as_view(template_name = "registration/login.html"), name = "login"),
     path("", PortalTemplateView.as_view(template_name = "index.html"), name = "index"),
     path("oglasi/", include(('oglasi.urls', 'oglasi'), namespace="oglasi"))
-    path('', include('account.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
